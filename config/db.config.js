89fc7f3e61350d021fe8787ccaae2d8d64508b5d
njayman <najishm828282@gmail.com>
2020-11-28 18:31:35 +0600

    deployment fixes, env, gitignores and distfile.

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,32 @@
+/*module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "noakhali",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};*/
+
+var dotenv = require('dotenv');
+dotenv.config()
+const { DBHOST, DBUSER, DBPASSWORD, DB } = process.env
+console.log(DBHOST, DBHOST, DBUSER, DBPASSWORD, DB)
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "noakhali",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: DBHOST,
+  PORT: 3306,
+  USER: DBUSER,
+  PASSWORD: DBPASSWORD,
+  DB: DB,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
