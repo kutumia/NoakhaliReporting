ed1f3ca92854481bc9ba4a917dc98c465f533600
kutumia <rhythmrahman007@gmail.com>
2021-01-17 18:11:43 +0600

    env

--- config/db.config.js
+++ config/db.config.js
@@ -1,27 +1,8 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "noakhali",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-
-// var dotenv = require('dotenv');
-// dotenv.config()
-// const { DBHOST, DBUSER, DBPASSWORD, DB } = process.env
-// //console.log(DBHOST, DBHOST, DBUSER, DBPASSWORD, DB)
 // module.exports = {
-//   HOST: DBHOST,
-//   PORT: 3306,
-//   USER: DBUSER,
-//   PASSWORD: DBPASSWORD,
-//   DB: DB,
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "noakhali",
 //   dialect: "mysql",
 //   pool: {
 //     max: 5,
@@ -30,3 +11,22 @@ module.exports = {
 //     idle: 10000
 //   }
 // };
+
+var dotenv = require('dotenv');
+dotenv.config()
+const { DBHOST, DBUSER, DBPASSWORD, DB } = process.env
+//console.log(DBHOST, DBHOST, DBUSER, DBPASSWORD, DB)
+module.exports = {
+  HOST: DBHOST,
+  PORT: 3306,
+  USER: DBUSER,
+  PASSWORD: DBPASSWORD,
+  DB: DB,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
